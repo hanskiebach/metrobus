84db68e4e1b4cb65110b144cfbff326d130bee00
Hans <kiebachh@vcu.edu>
2020-09-09 21:13:18 -0400

    Initial Commit

---
+++ check_users.py
@@ -0,0 +1,8 @@
+def check_users(current_users, new_users)
+    pass
+    
+
+if __name__ == "__main__":
+    current_us = ['chris', 'haritha', 'sally' , 'darnell' , 'superman']
+    new_us = ['george' , 'ringo' , 'superman' , 'hannibal']
+    check_users(current_us, new_us)
