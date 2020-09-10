4c79427d5aad475d605603e063cdde04db5ceb2f
Hans <kiebachh@vcu.edu>
2020-09-09 22:08:48 -0400

    Initial Commit

---
+++ Week 3/check_users.py
@@ -0,0 +1,12 @@
+def check_users(current_users, new_users): 
+    pass
+    for new_user in new_users:
+        if new_user in current_users:
+            print(f"Username is in use. Please enter a new username")
+        else:
+            print("Congratulations! This username is available")
+
+if __name__ == "__main__":
+    current_us = ['chris', 'haritha', 'sally' , 'darnell' , 'superman']
+    new_us = ['george' , 'ringo' , 'superman' , 'hannibal', 'matthew']
+    check_users(current_us, new_us)
